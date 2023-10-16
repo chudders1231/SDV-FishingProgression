a26c343d549a46c1d38a47eb0c0f538c1f828095
chudders1231 <chad_masters@hotmail.com>
2023-10-16 08:59:02 +0100

    Add project files.

---
+++ framework/PullFishFromWater.cs
@@ -0,0 +1,44 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Tools;
+using System;
+using StardewModdingAPI;
+using System.Runtime.CompilerServices;
+
+namespace FishingProgression.framework
+{
+    internal class PullFishFromWater
+    {
+
+        private static IMonitor Monitor;
+
+        // call this method from your Entry class
+        internal static void Initialize(IMonitor monitor)
+        {
+            Monitor = monitor;
+        }
+
+        // patches need to be static!
+        internal static bool pullFishFromWater_Prefix(FishingRod __instance, ref bool caughtDouble)
+        {
+            try
+            {
+                double rnd = Game1.random.Next(0, 100);
+                float chance = Math.Clamp(__instance.getLastFarmerToUse().FishingLevel * Globals.Config.DoubleHookChance, 0, 100);
+
+                Log.Debug($"RND: {rnd} - Chance: {chance} - FL: {__instance.getLastFarmerToUse().FishingLevel}");
+
+                if (Globals.Config.EnableDoubleHook && rnd <= chance)
+                {
+                    caughtDouble = true;
+                }   
+                return true; // don't run original logic
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log($"Failed in {nameof(pullFishFromWater_Prefix)}:\n{ex}", LogLevel.Error);
+                return true; // run original logic
+            }
+        }
+    }
+}
